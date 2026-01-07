e5f59c600202a842145e42743a532e2563254ae8
Masum Mahmud <mdmasumpresent@gmail.com>
2026-01-07 09:02:18 +0600

    authrize

--- dj/dj/settings.py
+++ dj/dj/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
     ]
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=3),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',)
